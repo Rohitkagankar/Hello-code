6c37e29ebfd4cac20315e6650783aa2c553d85ec
Rohit Rajaram Kagankar <112383067+Rohitkagankar@users.noreply.github.com>
2024-05-24 20:34:59 +0530

    simple page

--- 1 react/src/index.js
+++ 1 react/src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
     <div>
         <h1>hello world</h1>
+        <p>this is the first paragraph</p>
     </div>,document.getElementById("root")
 )
